4a8152b0d716d0a2583e8cbb4bf8475734153e66
Егор Гординский <yegorgordinskiy2002@gmail.com>
2022-10-07 14:12:00 +0800

    update

--- Project/Form1.cs
+++ Project/Form1.cs
@@ -12,9 +12,56 @@ namespace Project
 {
     public partial class Form1 : Form
     {
+        List<Particle> particles = new List<Particle>();
         public Form1()
         {
             InitializeComponent();
+            picDisplay.Image = new Bitmap(picDisplay.Width, picDisplay.Height);
+
+            for (var i = 0; i < 500; ++i)
+            {
+                var particle = new Particle();
+                particle.X = picDisplay.Image.Width / 2;
+                particle.Y = picDisplay.Image.Height / 2;
+                particles.Add(particle);
+            }
+        }
+
+        private void picDisplay_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void UpdateState()
+        {
+            foreach (var particle in particles)
+            {
+                var directionInRadians = particle.Direction / 180 * Math.PI;
+                particle.X += (float)(particle.Speed * Math.Cos(directionInRadians));
+                particle.Y -= (float)(particle.Speed * Math.Sin(directionInRadians));
+            }
+        }
+
+        // функция рендеринга
+        private void Render(Graphics g)
+        {
+            // утащили сюда отрисовку частиц
+            foreach (var particle in particles)
+            {
+                particle.Draw(g);
+            }
+        }
+        
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            UpdateState(); // каждый тик обновляем систему
+
+            using (var g = Graphics.FromImage(picDisplay.Image))
+            {
+                g.Clear(Color.White);
+                Render(g); // рендерим систему
+            }
+            picDisplay.Invalidate();
+
         }
     }
 }
