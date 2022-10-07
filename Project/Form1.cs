a4db777fd88dade5d005fc69d1c3711739e6bf7b
Егор Гординский <yegorgordinskiy2002@gmail.com>
2022-10-07 14:40:45 +0800

    Added Emitter

--- Project/Form1.cs
+++ Project/Form1.cs
@@ -13,6 +13,7 @@ namespace Project
     public partial class Form1 : Form
     {
         List<Particle> particles = new List<Particle>();
+        Emitter emitter = new Emitter();
         public Form1()
         {
             InitializeComponent();
@@ -23,81 +24,26 @@ namespace Project
         {
 
         }
-        private void UpdateState()
-        {
-            foreach (var particle in particles)
-            {
-                particle.Life -= 1;
-                if (particle.Life < 0)
-                {
-                    particle.Life = 20 + Particle.rand.Next(100);
-                    particle.X = MousePositionX;
-                    particle.Y = MousePositionY;
-
-                    var direction = (double)Particle.rand.Next(360);
-                    var speed = 1 + Particle.rand.Next(10);
-
-                    particle.SpeedX = (float)(Math.Cos(direction / 180 * Math.PI) * speed);
-                    particle.SpeedY = -(float)(Math.Sin(direction / 180 * Math.PI) * speed);
-  
-                    particle.Radius = 2 + Particle.rand.Next(10);
-                }
-                else
-                {
-                    
-                    particle.X += particle.SpeedX;
-                    particle.Y += particle.SpeedY;
-                }
-            }
-            for (var i = 0; i < 10; ++i)
-            {
-                if (particles.Count < 500)
-                {
-                    
-                    var particle = new ParticleColorful();
-                   
-                    particle.FromColor = Color.Yellow;
-                    particle.ToColor = Color.FromArgb(0, Color.Magenta);
-                    particle.X = MousePositionX;
-                    particle.Y = MousePositionY;
-                    particles.Add(particle);
-                }
-                else
-                {
-                    break;
-                }
-            }
-        }
-
-        // функция рендеринга
-        private void Render(Graphics g)
-        {
-            // утащили сюда отрисовку частиц
-            foreach (var particle in particles)
-            {
-                particle.Draw(g);
-            }
-        }
+        
         
         private void timer1_Tick(object sender, EventArgs e)
         {
-            UpdateState();
+            emitter.UpdateState();
 
             using (var g = Graphics.FromImage(picDisplay.Image))
             {
-                g.Clear(Color.Black); 
-                Render(g);
+                g.Clear(Color.Black);
+                emitter.Render(g);
             }
 
             picDisplay.Invalidate();
 
         }
-        private int MousePositionX = 0;
-        private int MousePositionY = 0;
+        
         private void picDisplay_MouseMove(object sender, MouseEventArgs e)
         {
-            MousePositionX = e.X;
-            MousePositionY = e.Y;
+            emitter.MousePositionX = e.X;
+            emitter.MousePositionY = e.Y;
         }
     }
 }
